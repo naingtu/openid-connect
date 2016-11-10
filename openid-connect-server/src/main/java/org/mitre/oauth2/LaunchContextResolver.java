2a68dfff76ce9fe1b93e974200423232d774e388
Angelo Kastroulis <angelo@kastroulis.com>
2016-11-10 13:11:29 -0500

    Oracle conversion

---
+++ openid-connect-server/src/main/java/org/mitre/oauth2/LaunchContextResolver.java
@@ -0,0 +1,13 @@
+
+package org.smartplatforms.oauth2;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Set;
+
+import org.smartplatforms.oauth2.LaunchContextEntity;
+
+public interface LaunchContextResolver {
+	public Serializable resolve(String launchId, Map<String,String> needs) throws NeedUnmetException;
+	public String getServiceURL();
+}
